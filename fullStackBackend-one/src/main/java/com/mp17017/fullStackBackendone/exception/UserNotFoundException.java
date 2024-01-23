b0c14db365ca9b11046fc04bd7125ed4543b6bac
Michael Perry <michaeleperr@gmail.com>
2024-01-23 17:10:04 -0500

    Fixed file structure

--- fullStackBackend-one/src/main/java/com/mp17017/fullStackBackendone/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mp17017.fullStackBackendone.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not find a user with the id " + id);
-    }
-}
--- fullStackBackend-one/src/main/java/com/mp17017/fullStackBackendone/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mp17017.fullStackBackendone.repository;
-
-import com.mp17017.fullStackBackendone.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
